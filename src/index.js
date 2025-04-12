2d34013ebe486c7a19c17cc22ccad7b347199508
Caroline Ininga <carolineininga@gmail.com>
2025-04-12 14:14:58 +0300

    completed fetch on demand lab

--- src/index.js
+++ src/index.js
@@ -1,4 +1,47 @@
 const init = () => {
+
+    const inputForm = document.querySelector("form")
+    inputForm.addEventListener("submit", (event)=>{
+        event.preventDefault()
+        const input = document.querySelector("input#searchByID")
+        console.log(input.value)
+       
+       fetch(`http://localhost:3000/movies/${input.value}`)
+        .then((Response) => Response.json())
+        .then((data)=> {
+
+            console.log(data)
+           
+            //create element to append title
+            let titleArea = document.querySelector("h4");
+            titleArea.innerHTML = ''; // Clear previous content
+            let titleName = document.createElement("h2");
+            titleName.innerText = data.title;
+            titleArea.append(titleName);
+            input.value = '';
+
+            // Create element to append summary
+            let summaryArea = document.querySelector("p");
+            summaryArea.innerHTML = ''; // Clear previous content
+            let summaryContent = document.createElement("p");
+            summaryContent.innerText = data.summary;
+            summaryArea.append(summaryContent);
+            input.value = '';
+
+
+        })
+
+
+
+
+
+
+
+    }
+    
+    )
+
+
   
 }
 
